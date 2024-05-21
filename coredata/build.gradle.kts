2f455051c5acecedf8a5121062baf3453fbdb15c
Ioannis Anyfantakis <ianyfantakis@deloitte.gr>
2024-05-21 18:36:16 +0300

    Restore test dependencies on coredata module

--- coredata/build.gradle.kts
+++ coredata/build.gradle.kts
@@ -42,4 +42,8 @@ dependencies {
     implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("androidx.security:security-crypto:1.1.0-alpha06") // encrypted shared preferences
     implementation("androidx.datastore:datastore-preferences:1.1.1")  // data store preferences
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
