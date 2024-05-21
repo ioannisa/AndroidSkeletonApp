73161a7d202d3737a4ae0c3db6f792b21c5de6fa
Ioannis Anyfantakis <ianyfantakis@deloitte.gr>
2024-05-21 16:26:35 +0300

    Libraries update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,20 +86,20 @@ android {
     }
 }
 
-val lifecycleVer = "2.7.0"
+val lifecycleVer = "2.8.0"
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVer")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.benchmark:benchmark-common:1.2.3")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.benchmark:benchmark-common:1.2.4")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(project(":coredata"))
 
     // CUSTOM IMPLEMENTATIONS START ----->
@@ -110,7 +110,7 @@ dependencies {
     flipper()
 
     // https://coil-kt.github.io/coil/compose/
-    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
@@ -131,7 +131,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
