04bebe88217bfccfd0cd16000f37a861acc7854f
Ioannis Anyfantakis <ianyfantakis@deloitte.gr>
2024-05-21 16:33:47 +0300

    Libraries update

--- coredata/build.gradle.kts
+++ coredata/build.gradle.kts
@@ -35,19 +35,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
 
-    // START
-        implementation("com.squareup.retrofit2:retrofit:2.9.0")
-        implementation("com.google.code.gson:gson:2.10.1")
-        implementation("com.jakewharton.timber:timber:5.0.1")
-        implementation("androidx.security:security-crypto:1.1.0-alpha06") // encrypted shared preferences
-        implementation("androidx.datastore:datastore-preferences:1.0.0")  // data store preferences
-    // FINISH
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation("androidx.security:security-crypto:1.1.0-alpha06") // encrypted shared preferences
+    implementation("androidx.datastore:datastore-preferences:1.1.1")  // data store preferences
 }
